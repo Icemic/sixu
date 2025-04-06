1eb526565587e647649665500efb9cc327b9fa70
Icemic <bingfeng.web@gmail.com>
2025-04-07 07:53:56 +0800

    feat: add multi-line comment parsing support

--- src/parser/comment.rs
+++ src/parser/comment.rs
@@ -9,7 +9,23 @@ use crate::result::SixuResult;
 
 /// parse comment like `// C++/EOL-style comments`
 pub fn comment(input: &str) -> SixuResult<&str, &str> {
+    alt((comment_single, comment_multi))(input)
+}
+
+/// parse comment like `// C++/EOL-style comments`
+pub fn comment_single(input: &str) -> SixuResult<&str, &str> {
     preceded(tag("//"), cut(is_not("\r\n")))(input)
+}
+
+/**
+ * parse comment like `/* C-style comments */`
+ * allows multiple * at the beginning and end of the comment
+ * and allows multiple lines in the comment, including starts with `*` and ends with `*`
+ * and allows empty comment, which is `/* */`
+ * returns purely the comment content, without `/*` and `*/`
+*/
+pub fn comment_multi(input: &str) -> SixuResult<&str, &str> {
+    delimited(tag("/*"), take_until("*/"), tag("*/"))(input)
 }
 
 /// match contiguous comments or whitespaces, which can be multiple lines
@@ -22,24 +38,11 @@ pub fn span0(input: &str) -> SixuResult<&str, ()> {
 
 /// match contiguous comments or whitespaces, which is only one line
 pub fn span0_inline(input: &str) -> SixuResult<&str, ()> {
-    value(
-        (),
-        many0(alt((map(comment, |_| ()), value((), space1)))),
-    )(input)
-}
-
-/// match contiguous comments or whitespaces, which can be multiple lines
-pub fn span1(input: &str) -> SixuResult<&str, ()> {
-    value(
-        (),
-        many1(alt((map(comment, |_| ()), value((), multispace1)))),
-    )(input)
+    value((), many0(alt((map(comment, |_| ()), value((), space1)))))(input)
 }
 
 #[cfg(test)]
 mod tests {
-    use nom::error::{VerboseError, VerboseErrorKind};
-    use nom::Err;
 
     use super::*;
 
@@ -49,6 +52,21 @@ mod tests {
         assert_eq!(comment("// comment\n"), Ok(("\n", " comment")));
         assert_eq!(comment("// comment\nnext"), Ok(("\nnext", " comment")));
         assert_eq!(comment("// comment\nnext\n"), Ok(("\nnext\n", " comment")));
+    }
+
+    #[test]
+    fn test_comment_multi() {
+        assert_eq!(comment_multi("/* comment */"), Ok(("", " comment ")));
+        assert_eq!(comment_multi("/** comment */"), Ok(("", "* comment ")));
+        assert_eq!(
+            comment_multi("/* comment \n next */"),
+            Ok(("", " comment \n next "))
+        );
+        assert_eq!(
+            comment_multi("/* comment \n * next */"),
+            Ok(("", " comment \n * next "))
+        );
+        assert_eq!(comment_multi("/* \n */"), Ok(("", " \n ")));
     }
 
     #[test]
@@ -79,21 +97,5 @@ mod tests {
         assert_eq!(span0("\t\t\t"), Ok(("", ())));
         assert_eq!(span0("\r\r\r"), Ok(("", ())));
         assert_eq!(span0("  \n  \n  "), Ok(("", ())));
-    }
-
-    #[test]
-    fn test_comment_or_multispace1() {
-        assert_eq!(
-            span1(""),
-            Err(Err::Error(VerboseError {
-                errors: vec![
-                    ("", VerboseErrorKind::Nom(nom::error::ErrorKind::MultiSpace)),
-                    ("", VerboseErrorKind::Nom(nom::error::ErrorKind::Alt)),
-                    ("", VerboseErrorKind::Nom(nom::error::ErrorKind::Many1))
-                ]
-            }))
-        );
-        assert_eq!(span1(" "), Ok(("", ())));
-        assert_eq!(span1("\n"), Ok(("", ())));
     }
 }
